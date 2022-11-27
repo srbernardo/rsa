6d5ce28e662531e512c811d073517214bcf7a3b7
Arthurpmrs <arthurpmrs@gmail.com>
2022-11-27 13:06:51 -0300

    Mudanças para suportar valores de p e q muito grandes.

--- Func/decriptar.py
+++ Func/decriptar.py
@@ -42,13 +42,13 @@ def mainDecriptar():
   phi = (p-1)*(q-1)
   n = p*q
 
-  arquivo = open('./bruno.txt', 'r')
-  msg = arquivo.read().split(" ")
+  with open('./bruno.txt', 'r') as arquivo:
+    msg = arquivo.read().split(" ")
 
-  msg_decriptada = []
+    msg_decriptada = []
 
-  for i in range(0,len(msg)):
-    msg_decriptada.append( (int(msg[i]) ** inverso(e,phi)) % n)
+    for i in range(0,len(msg)):
+      msg_decriptada.append(pow(int(msg[i]), inverso(e, phi), n))
 
   alfabeto = ['a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v','w', 'x','y','z',' ']
   alfabeto_numero = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
